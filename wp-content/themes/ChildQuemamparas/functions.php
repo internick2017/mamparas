245a3320da4929ca866efee8dba9ab5a21db10e6
Daniel Granados <daniel@ceicom.com.br>
2021-09-29 09:55:57 -0300

    actual function php and style.css

---
+++ wp-content/themes/ChildQuemamparas/functions.php
@@ -0,0 +1,39 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+// END ENQUEUE PARENT ACTION
+
+// Quitar el rango de precios de los productos variables en woocommerce
+ 
+function precio_desde( $price, $product ) {
+    // Precio normal
+    $prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
+    $price = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+ 
+    // Precio rebajado
+    $prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
+    sort( $prices );
+    $saleprice = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+ 
+    if ( $price !== $saleprice ) {
+        $price = '<del>' . $saleprice . '</del> <ins>' . $price . '</ins>';
+    }
+   
+    return $price;
+}
+ 
+add_filter( 'woocommerce_variable_sale_price_html', 'precio_desde', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 'precio_desde', 10, 2 );
